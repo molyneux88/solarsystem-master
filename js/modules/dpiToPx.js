779a005559e3bc8a9a62beb6f23dd28e26a846c8
luke.molyneux <molyneux_88@hotmail.com>
2026-02-09 16:32:46 +0100

    Revert

--- js/modules/dpiToPx.js
+++ js/modules/dpiToPx.js
@@ -4,10 +4,11 @@ export function mmToPx(mm) {
 
   const dppx =
     window.devicePixelRatio ||
-            (window.matchMedia && 
-                window.matchMedia(
-                    "(min-resolution: 2dppx), (-webkit-min-device-pixel-ratio: 1.5),(-moz-min-device-pixel-ratio: 1.5),(min-device-pixel-ratio: 1.5)").matches? 2 : 1) ||
-            1;
+    (window.matchMedia &&
+      window.matchMedia("(min-resolution: 2dppx)").matches
+      ? 2
+      : 1) ||
+    1;
 
-  return (dpi / dppx) *0.5393701;
+  return (dpi / dppx) * (mm / 25.4);
 }
